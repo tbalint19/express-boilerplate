08aeb03405a6bb6abd07a39defbd5af89d98fce9
tbalint19 <toth910719balint@gmail.com>
2019-11-08 17:11:54 +0100

    error handler

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -1,9 +1,20 @@
-var errorHandler = (err, req, res, next) => {
-  res.status(err.status || 500)
-  res.json({
-    message: err.message,
-    error: process.env.NODE_ENV === 'development' ? err : {},
-  })
+const getStatus = (err) => {
+  if (err.status) return err.status
+  else if (err.name && err.name === 'SequelizeValidationError') return 400
+  // ...
+  else return 500
+}
+
+const getContent = (err) => {
+  let data = {}
+  data.message = err.message
+  if (process.env.NODE_ENV !== 'production') data.error = err
+  return data
+}
+
+const errorHandler = (err, req, res, next) => {
+  res.status(getStatus(err))
+  res.json(getContent(err))
 }
 
 module.exports = errorHandler
