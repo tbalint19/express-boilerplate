49e2bbaf1d64d3c58272202bbf732ca989f38bbe
Tóth <tothbalint719@gmail.com>
2019-12-25 14:17:21 +0100

    sonar config

---
+++ CI/sonar/util/url.js
@@ -0,0 +1,18 @@
+const config = require('../config.json')
+
+const bugUrl = (config) => {
+  const url = config["sonar.host.url"]
+  const org = config["sonar.organization"]
+  return `${url}/api/issues/search?organization=${org}&pageSize=-1&p=1&resolved=false`
+}
+
+const duplicationsUrl = (config) => {
+  const url = config["sonar.host.url"]
+  const key = config["sonar.projectKey"]
+  return `${url}/api/measures/component_tree?baseComponentKey=${key}&metricKeys=duplicated_lines&pageSize=500&p=1`
+}
+
+module.exports = {
+  bugUrl: bugUrl(config),
+  duplicationsUrl: duplicationsUrl(config)
+}
